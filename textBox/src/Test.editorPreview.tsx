06e21d7acc9c8b39963590ab4fe929e87ca141f4
s2651718 <s.lysin@student.utwente.nl>
2024-04-23 10:48:55 +0200

    Second commit

--- textBox/src/Test.editorPreview.tsx
+++ textBox/src/Test.editorPreview.tsx
@@ -1,5 +1,4 @@
 import { ReactElement, createElement } from "react";
-// import { HelloWorldSample } from "./components/HelloWorldSample";
 import { TestPreviewProps } from "../typings/TestProps";
 
 export function preview({  }: TestPreviewProps): ReactElement {
